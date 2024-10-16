e8f00b9241a0b6d2f70946cbce9422a540390539
Oksana Kalynivska <o.kalunivska@gmail.com>
2024-10-16 10:25:09 +0300

    Add docstring to the method

--- appendToListFunction.py
+++ appendToListFunction.py
@@ -1,4 +1,15 @@
 def appendToList(existing_list, value_being_added):
+    """ Append values to existing list
+
+    Args:
+        existing_list (list): list to which values will be added
+        value_being_added (int, float, str, tuple, list, bool): value
+            or values to be added to the list
+
+    Returns:
+        list: Updated list
+    """
+
     if (isinstance(value_being_added, list)) or (isinstance(value_being_added, tuple)):
         existing_list.extend(value_being_added)
     else:
@@ -8,11 +19,18 @@ def appendToList(existing_list, value_being_added):
 
 list_of_numbers = [1, 2, 3]
 second_list_of_numbers = [1, 23, 56]
-single_value = 3
-str_single_value = "test_value"
+int_single_value = 3
+bool_single_value = True
+str_single_value = 'test_value'
 tuple_values = (1, 4, 8, 0)
 
-print(appendToList(list_of_numbers, single_value))
-print(appendToList(list_of_numbers, tuple_values))
-print(appendToList(list_of_numbers, str_single_value))
-print(appendToList(list_of_numbers, second_list_of_numbers))
+print(f"int {int_single_value} added:", appendToList(
+    list_of_numbers, int_single_value))
+print(f"tuple {tuple_values} added:",
+      appendToList(list_of_numbers, tuple_values))
+print(f"str {str_single_value} added:", appendToList(
+    list_of_numbers, str_single_value))
+print(f"list {second_list_of_numbers} added:",
+      appendToList(list_of_numbers, second_list_of_numbers))
+print(f"bool {bool_single_value} added:",
+      appendToList(list_of_numbers, bool_single_value))
