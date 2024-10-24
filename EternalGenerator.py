4b50ead379513bdb97793f07dea75a9cf2c5fdd0
Oksana Kalynivska <o.kalunivska@gmail.com>
2024-10-24 12:02:12 +0300

    Add eternal generator

--- EternalGenerator.py
+++ EternalGenerator.py
@@ -12,8 +12,8 @@ list_str = ['Java', 'Python', 'C++', 'C#', 'Pascal']
 
 gen = eternal_generator(list_str)
 print(type(gen))  # <class 'generator'>
-for _ in range(10):
-    print(next(gen))
+# for _ in range(10):
+#     print(next(gen))
 
 # Java
 # Python
@@ -25,3 +25,28 @@ for _ in range(10):
 # Pascal
 # C#
 # C++
+
+for _ in range(10):
+    print(gen.send(None))
+# Java
+# Python
+# C++
+# C#
+# Pascal
+# C++
+# C#
+# Pascal
+# Python
+# Pascal
+
+# differnt order for print(gen.send(_))
+# Python
+# C#
+# C++
+# Java
+# C#
+# C++
+# C++
+# Java
+# Java
+# Python
